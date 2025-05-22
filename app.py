7a4a9336d12898e099ae2ff1f570b221f6054350
stars0527 <domzfalcon@gmail.com>
2025-05-22 10:32:17 +0800

    Fix app to use Render port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 
@@ -18,4 +19,5 @@ def webhook():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
